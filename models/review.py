b6a6f6459ce2e99a63b886b60969b3e0d9dc9aab
sylviajebet <sylviajebett@gmail.com>
2022-09-03 23:22:21 +0300

    added classes

---
+++ models/review.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" Review module """
+
+from models.base_model import BaseModel
+
+
+class Review(BaseModel):
+    """review class"""
+    place_id = ""
+    user_id = ""
+    text = ""
